be915199075393cc9b226d702a15d12c1d26fbda
Mohammad-Fahim-Hossain <mohammad.fahim@northsouth.edu>
2020-07-27 20:50:26 +0600

    Upload the project.

---
+++ AR-Children's-Book/Assets/SecneManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SecneManager : MonoBehaviour
+{
+    public void QuitApp() {
+
+        Application.Quit();
+
+    }
+    public void BanglaAlpha() {
+        SceneManager.LoadScene("BanglaAlpha");
+    }
+    public void BanglaFruit()
+    {
+        SceneManager.LoadScene("BanglaFruit");
+    }
+    public void BanglaVideo()
+    {
+        SceneManager.LoadScene("BanglaVideo");
+    }
+
+
+    public void EnglishAlpha()
+    {
+        SceneManager.LoadScene("EnglishAlpha");
+    }
+    public void EnglishFruit()
+    {
+        SceneManager.LoadScene("EnglishFruit");
+    }
+    public void EnglishVideo()
+    {
+        SceneManager.LoadScene("EnglishVideo");
+    }
+    public void Mainmanu()
+    {
+        SceneManager.LoadScene("UI");
+
+    }
+
+
+
+}
